b5c28d551a46e405afd6207926bc52c517a21331
july2m3 <july2m3@gmail.com>
2020-05-16 11:56:08 -0500

    Beginning hex grid

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,61 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import Canvas from './Canvas';
+import './style.css';
+
+const size = 10;
+
+class App extends React.Component {
+  drawTile = (x: number, y: number, color = '#333') => {
+    let canvas = document.querySelector('canvas')?.getContext('2d')!;
+    let side = 0;
+
+    // canvas.rotate((90 * Math.PI) / 180);
+    // canvas.rotate((-90 * Math.PI) / 180);
+
+    if (canvas != null) {
+      canvas.beginPath();
+      //   canvas.moveTo(x + 10 + size * Math.cos(0), y + size * Math.sin(0));
+      canvas.moveTo(x + size, y + size);
+
+      for (side; side < 7; side++) {
+        canvas.lineTo(
+          x + size * Math.cos((side * 2 * Math.PI) / 6),
+          y + size * Math.sin((side * 2 * Math.PI) / 6),
+        );
+      }
+
+      canvas.fillStyle = color;
+      canvas.fill();
+      canvas.setTransform(1, 0, 0, 1, 0, 0);
+    }
+  };
+
+  componentDidMount() {
+    let color = 'blue';
+    let xOffset = 0;
+    let yOffset = 0;
+
+    for (let i = 0; i < 30; i++) {
+      color = 'blue';
+
+      for (let j = 0; j < 10; j++) {
+        color = j % 2 === 0 ? 'green' : 'blue';
+        xOffset = i % 2 === 0 ? 0 : 10;
+        yOffset = j % 2 === 0 ? 0 : 0;
+
+        this.drawTile(i * 10 + xOffset, j * 18 + yOffset, color);
+      }
+    }
+  }
+
+  render() {
+    return (
+      <div>
+        <h1>Army Game</h1>
+        <Canvas />
+      </div>
+    );
+  }
 }
-
 export default App;
---
+++ src/Canvas.tsx
@@ -0,0 +1,8 @@
+import React from 'react';
+
+const Canvas = () => {
+  //   return <canvas id="canvas" width="200" height="200"></canvas>;
+  return <canvas id="canvas"> </canvas>;
+};
+
+export default Canvas;

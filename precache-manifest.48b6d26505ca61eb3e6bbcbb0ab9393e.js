e812627a51763dd323b49c8f102198c3a089c1b7
Julio <july2m3@gmail.com>
2020-05-17 18:59:44 -0500

    Updates

---
+++ precache-manifest.48b6d26505ca61eb3e6bbcbb0ab9393e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "269b6e74bbe9f2aa0ee70fa207996efa",
+    "url": "/hexagon-map-creator/index.html"
+  },
+  {
+    "revision": "23b38cd5cf7e7c8bee15",
+    "url": "/hexagon-map-creator/static/css/main.930cdb94.chunk.css"
+  },
+  {
+    "revision": "8823c77a4260edb26701",
+    "url": "/hexagon-map-creator/static/js/2.18465c13.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/hexagon-map-creator/static/js/2.18465c13.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "23b38cd5cf7e7c8bee15",
+    "url": "/hexagon-map-creator/static/js/main.e1ef06b8.chunk.js"
+  },
+  {
+    "revision": "74fa4b5fb9b3893c33cb",
+    "url": "/hexagon-map-creator/static/js/runtime-main.130e2934.js"
+  },
+  {
+    "revision": "b3f0e1f543e6cbe9e2b7f7654fa65ee7",
+    "url": "/hexagon-map-creator/static/media/borderless.b3f0e1f5.png"
+  }
+]);
